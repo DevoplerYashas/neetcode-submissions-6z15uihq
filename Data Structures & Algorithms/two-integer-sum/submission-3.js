49bc424c8f24ccfb1381205b9129c6224a1611d8
DevoplerYashas <yashas.developer@gmail.com>
2026-04-12 00:31:48 +0530

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        if (nums.length === 0) return false;
+
+        const indexObj = {};
+        const res = [];
+
+        for(let i=0; i<nums.length; i++) {
+            indexObj[nums[i]] = i;
+        }
+
+        for (let i=0; i < nums.length; i++) {
+            const resIndex = indexObj[target - nums[i]];
+
+            if(resIndex !== undefined && i !== resIndex && resIndex > i){
+                res.push([i, resIndex]);
+
+                break;
+            }
+        }
+
+        return res[0];
+    }
+}
