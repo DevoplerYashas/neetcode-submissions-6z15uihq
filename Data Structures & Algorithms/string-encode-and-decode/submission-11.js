e5b5c96575ddc11aa4c00c505b2c8611ce4ada38
DevoplerYashas <yashas.developer@gmail.com>
2026-04-14 01:50:59 +0530

    Add: string-encode-and-decode - submission-11

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-11.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let res = '';
+        for (const e of strs) {
+            res += e.length + '#' + e;
+        }
+        return res;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let i = 0;
+        const finalRes = [];
+
+        while (i < str.length) {
+            let j = i;
+            while (str[j] != '#') {
+                j++;
+            }
+            const curWordLen = str.slice(i, j);
+            i = j + 1;
+            j = i + parseInt(curWordLen);
+
+            finalRes.push(str.slice(i, j));
+            i = j;
+        }
+
+        return finalRes;
+    }
+}
